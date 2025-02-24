dd9ea0d185bcacb9c1aed9bd19c34ecb07ab4727
Anowar Shajib <ajshajib@uchicago.edu>
2025-02-23 23:18:31 -0600

    Enhance WiggleCleaner class with additional parameters for curve fitting and plotting functionality

--- src/raccoon/wiggle_cleaner.py
+++ src/raccoon/wiggle_cleaner.py
@@ -13,6 +13,8 @@ class WiggleCleaner(object):
     def __init__(
         self,
         wavelengths,
+        datacube,
+        noise_cube,
         gaps,
         n_amplitude=9,
         n_frequency=9,
@@ -33,6 +35,8 @@ class WiggleCleaner(object):
         :type n_offset: int
         """
         self._wavelengths = jnp.array(wavelengths)
+        self._datacube = datacube
+        self._noise_cube = noise_cube
         self._gaps = jnp.array(gaps)
         self._n_amplitude = n_amplitude
         self._n_frequency = n_frequency
@@ -54,7 +58,9 @@ class WiggleCleaner(object):
         """
         return self.scale_wavelengths_to_1_m1(self._wavelengths)
 
-    def wiggle_func(self, xs, frequency_params, amplitude_params, offset_params, phi):
+    def wiggle_func(
+        self, xs, frequency_params, amplitude_params, offset_params, phi, k_1=0, k_2=0
+    ):
         """
         Get the wiggle function.
 
@@ -74,8 +80,15 @@ class WiggleCleaner(object):
         frequency = jnp.abs(jnp.polyval(frequency_params, xs))
         amplitude = jnp.abs(jnp.polyval(amplitude_params, xs))
         offset = jnp.polyval(offset_params, xs)
+        # k_1 = 0.0
+        # k_2 = 0.0
+        wave_function = (
+            jnp.sin(frequency * xs + phi)
+            + k_1 * (jnp.sin(frequency * xs + phi) ** 2 - np.pi * frequency)
+            + k_2 * jnp.sin(3 * (frequency * xs + phi))
+        )
 
-        return 1.0 + amplitude * jnp.sin(frequency * xs + phi) + offset
+        return 1.0 + amplitude * wave_function + offset
 
     def scale_wavelengths_to_1_m1(self, w):
         """
@@ -106,10 +119,20 @@ class WiggleCleaner(object):
         frequency_params = params[: n_frequency + 1]
         phi_0 = params[n_frequency + 1]
         amplitude_params = params[n_frequency + 2 : n_amplitude + n_frequency + 3]
-        offset_params = params[-n_offset - 1 :]
+        offset_params = params[
+            n_amplitude + n_frequency + 3 : n_amplitude + n_frequency + n_offset + 4
+        ]
+        k_1 = params[-2]
+        k_2 = params[-1]
 
         model = self.wiggle_func(
-            self.scaled_w, frequency_params, amplitude_params, offset_params, phi_0
+            self.scaled_w,
+            frequency_params,
+            amplitude_params,
+            offset_params,
+            phi_0,
+            k_1=k_1,
+            k_2=k_2,
         )
         return model
 
@@ -195,8 +218,9 @@ class WiggleCleaner(object):
         n_amplitude=None,
         n_frequency=None,
         n_offset=None,
-        verbose=False,
         proximity_threshold=200,
+        plot=False,
+        plot_amplitude_offset=False,
     ):
         """
         Fit the curve.
@@ -211,6 +235,12 @@ class WiggleCleaner(object):
         :type n_frequency: int
         :param n_offset: Number of offset parameters
         :type n_offset: int
+        :param proximity_threshold: Proximity lower limit in Angstrom for initial identifaction of peaks and troughs
+        :type proximity_threshold: float
+        :param plot: If True, plot the results
+        :type plot: bool
+        :param plot_amplitude_offset: If True, plot the amplitude and offset
+        :type plot_amplitude_offset: bool
         :return: Fitted parameters
         :rtype: jax.numpy.array
         """
@@ -253,6 +283,7 @@ class WiggleCleaner(object):
                 np.array([init_phi]),
                 init_amplitude_params,
                 init_offset_params,
+                np.array([0, 0]),
             ]
         )
         x0 = jnp.array(x0)
@@ -268,51 +299,232 @@ class WiggleCleaner(object):
         res = solver.run(x0)
         res_params = res.params
 
-        if verbose:
-            # plt.plot(
-            #     self._wavelengths, jnp.polyval(init_amplitude_params, self.scaled_w)
-            # )
-            # plt.plot(
-            #     self._wavelengths, jnp.polyval(init_frequency_params, self.scaled_w)
-            # )
-            # # plt.plot(self._wavelengths, jnp.polyval(init_offset_params, self.scaled_w))
-            # plt.show()
-
-            plt.plot(
-                self._wavelengths,
+        if plot:
+            self.plot_model(
                 curve,
-                label="Input",
-                ls="None",
-                marker="o",
-                markersize=2,
+                noise,
+                n_amplitude,
+                n_frequency,
+                n_offset,
+                res_params,
+                plot_amplitude_offset,
+                x0,
             )
-            plt.plot(self._wavelengths, self.model(res_params), label="Model", lw=2)
-            for g in self._gaps:
-                plt.axvspan(g[0], g[1], color="black", alpha=0.1)
-            offset_params = res_params[-n_offset - 1 :]
+
+        return res_params
+
+    def plot_model(
+        self,
+        curve,
+        noise,
+        n_amplitude,
+        n_frequency,
+        n_offset,
+        res_params,
+        plot_amplitude_offset=False,
+        x0=None,
+    ):
+        red = "#e41a1c"
+        blue = "#377eb8"
+        green = "#4daf4a"
+        purple = "#984ea3"
+        orange = "#ff7f00"
+        yellow = "#ffff33"
+
+        plt.errorbar(
+            self._wavelengths,
+            curve,
+            yerr=noise,
+            label="Input",
+            ls="None",
+            marker="o",
+            markersize=2,
+            alpha=0.2,
+            c=blue,
+        )
+        plt.plot(
+            self._wavelengths,
+            curve,
+            label="Input",
+            ls="None",
+            marker="o",
+            markersize=2,
+            c=blue,
+            alpha=0.6,
+        )
+        plt.plot(
+            self._wavelengths, self.model(res_params), label="Model", lw=2, c=orange
+        )
+        for g in self._gaps:
+            plt.axvspan(g[0], g[1], color="black", alpha=0.1)
+
+        if plot_amplitude_offset:
+            offset_params = res_params[
+                n_frequency + n_amplitude + 3 : n_frequency + n_amplitude + n_offset + 4
+            ]
             plt.plot(
                 self._wavelengths,
                 1 + jnp.polyval(offset_params, self.scaled_w),
                 label="Offset",
+                c=green,
             )
             amplitude_params = res_params[
-                n_frequency + 2 : n_amplitude + n_frequency + 3
+                n_frequency + 2 : n_frequency + n_amplitude + 3
             ]
             plt.plot(
                 self._wavelengths,
-                1
+                1.0
                 + jnp.abs(jnp.polyval(amplitude_params, self.scaled_w))
                 + jnp.polyval(offset_params, self.scaled_w),
                 label="Amplitude",
+                c=red,
             )
+
+        if x0 is not None:
             plt.plot(
                 self._wavelengths,
                 self.model(x0),
                 ls=":",
                 label="Init",
+                c=purple,
             )
-            plt.xlabel("Wavelengths")
-            plt.ylabel("Curve")
-            plt.legend()
-            plt.show()
-        return res_params
+
+        plt.xlabel("Wavelengths")
+        plt.ylabel("Curve")
+        plt.legend()
+        plt.ylim(np.min(curve) * 0.9, np.max(curve) * 1.1)
+        plt.show()
+
+    def get_modulaion_curve(self, x, y, aperture=4):
+        """
+        Get the modulation curve.
+
+        :param spaxel_x: Spaxel x
+        :type spaxel_x: int
+        :param spaxel_y: Spaxel y
+        :type spaxel_y: int
+        :param aperture: aperture size to sum the spectra to average out the wiggles
+        :type aperture: int
+        :return: Modulation curve
+        :rtype: np.ndarray
+        """
+        spectra = self._datacube[:, x, y]
+        noise = self._noise_cube[:, x, y]
+
+        # make circular mask around the pixel with radius s
+        mask = np.zeros_like(self._datacube[0], dtype=bool)
+
+        for i in range(x - 2 * aperture, x + 2 * aperture):
+            for j in range(y - 2 * aperture, y + 2 * aperture):
+                if (i - x) ** 2 + (j - y) ** 2 <= aperture**2:
+                    mask[i, j] = True
+
+        aperture_spectra = np.nansum(self._datacube[:, mask], axis=(1))
+        aperture_spectra /= np.nanmax(aperture_spectra) / np.nanmax(
+            self._datacube[:, x, y]
+        )
+        aperture_noise = np.sqrt(np.nansum(self._noise_cube[:, mask] ** 2, axis=(1)))
+        aperture_noise /= np.nanmax(aperture_noise) / np.nanmax(noise)
+
+        ratio = spectra / aperture_spectra
+        ratio_noise = np.abs(
+            np.sqrt((noise / spectra) ** 2 + (aperture_noise / aperture_spectra) ** 2)
+            * ratio
+        )
+
+        return ratio, ratio_noise
+
+    def fit_curve_with_best_bic(
+        self,
+        curve,
+        noise=None,
+        n_amplitude=None,
+        n_frequency=None,
+        n_offset=None,
+        min_n_amplitude=1,
+        proximity_threshold=200,
+        plot=False,
+        plot_amplitude_offset=False,
+    ):
+        """
+        Fit the curve with selecting amplitude polynomial order based on BIC.
+
+        :param curve: Curve
+        :type curve: np.ndarray
+        :param noise: Noise
+        :type noise: np.ndarray
+        :param n_amplitude: Maximum number of amplitude parameters
+        :type n_amplitude: int
+        :param n_frequency: Number of frequency parameters
+        :type n_frequency: int
+        :param n_offset: Number of offset parameters
+        :type n_offset: int
+        :param min_n_amplitude: Minimum number of amplitude parameters
+        :type min_n_amplitude: int
+        :param proximity_threshold: Proximity lower limit in Angstrom for initial identifaction of peaks and troughs
+        :type proximity_threshold: float
+        :param plot: If True, plot the results
+        :type plot: bool
+        :param plot_amplitude_offset: If True, plot the amplitude and offset
+        :type plot_amplitude_offset: bool
+        :return: Fitted parameters
+        :rtype: jax.numpy.array
+        """
+        print("Computing BIC for choices of n_amplitude...")
+        for i in range(min_n_amplitude, n_amplitude):
+            res_params = self.fit_curve(
+                curve,
+                noise,
+                n_amplitude=i,
+                n_frequency=n_frequency,
+                n_offset=n_offset,
+                proximity_threshold=proximity_threshold,
+                plot=False,
+                plot_amplitude_offset=False,
+            )
+
+            bic = self.get_bic(curve, noise, res_params)
+            print(f"n_amplitude: {i}, BIC: {bic}")
+            if i == min_n_amplitude:
+                best_n_amplitude = i
+                best_bic = bic
+                best_params = res_params
+            else:
+                if bic < best_bic:
+                    best_n_amplitude = i
+                    best_bic = bic
+                    best_params = res_params
+
+        print("Best n_amplitude: ", best_n_amplitude)
+        self._n_amplitude = best_n_amplitude
+
+        if plot:
+            self.plot_model(
+                curve,
+                noise,
+                best_n_amplitude,
+                n_frequency,
+                n_offset,
+                best_params,
+                plot_amplitude_offset,
+            )
+
+        return best_params
+
+    def get_bic(self, curve, noise, res_params):
+        """
+        Get the BIC.
+
+        :param curve: Curve
+        :type curve: np.ndarray
+        :param noise: Noise
+        :type noise: np.ndarray
+        :param res_params: Fitted parameters
+        :type res_params: jax.numpy.array
+        :return: BIC
+        :rtype: float
+        """
+        n = len(curve)
+        k = len(res_params)
+        loss = self.loss(res_params, curve, noise)
+        return n * np.log(loss) + k * np.log(n)
