0009585ba92477936d85e33ecf8db68ac1ca54a3
Anowar Shajib <ajshajib@uchicago.edu>
2025-07-08 17:00:44 -0500

    Revert version number to 0.0.0 in setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,6 @@ setup(
     packages=find_packages(where=".", include=["raccoon", "raccoon.*"]),
     package_dir={"": "."},
     url="https://github.com/ajshajib/raccoon",
-    version="0.1.0",
+    version="0.0.0",
     zip_safe=False,
 )
