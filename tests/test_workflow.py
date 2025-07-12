290b64b26aac0bab39a230446841e4e7a24a7e49
Anowar Shajib <ajshajib@uchicago.edu>
2025-07-11 21:41:07 -0500

    Enhance fit_wiggle and clean_cube tests; add error handling for invalid parameters and improve plotting options

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -108,7 +108,7 @@ class TestWorkflow:
             aperture_radius=aperture_radius,
             annulus_outer_radius=aperture_radius + 1,
             annulus_inner_radius=aperture_radius - 1,
-            plot=False,
+            plot=True,
             n_amplitude=5,
             n_frequency=3,
             min_n_amplitude=5,
@@ -172,6 +172,56 @@ class TestWorkflow:
             fit_full_model=True,
         )
 
+        with pytest.raises(ValueError):
+            self.wcleaner.fit_wiggle_with_model_selection(
+                15,  # quasar_x
+                15,  # quasar_y
+                aperture_radius=aperture_radius,
+                annulus_outer_radius=aperture_radius + 1,
+                annulus_inner_radius=aperture_radius - 1,
+                plot=False,
+                n_amplitude=5,
+                n_frequency=3,
+                min_n_amplitude=5,
+                min_n_frequency=1,
+                selection_criteria="bic",
+                init_peak_detection_proximity_threshold=200,
+                include_scatter=False,
+                outlier_rejection_method="sigma_clip",
+                sigma_clip=3,
+                sigma_clip_max_iterations=2,
+                use_huber_loss=True,
+                fdr_alpha=0.01,
+                fdr_outlier_max_fraction=0.10,
+                extract_covariance=True,
+                fit_full_model=True,
+            )
+
+        with pytest.raises(ValueError):
+            self.wcleaner.fit_wiggle_with_model_selection(
+                15,  # quasar_x
+                15,  # quasar_y
+                aperture_radius=aperture_radius,
+                annulus_outer_radius=aperture_radius + 1,
+                annulus_inner_radius=aperture_radius - 1,
+                plot=False,
+                n_amplitude=5,
+                n_frequency=3,
+                min_n_amplitude=1,
+                min_n_frequency=2,
+                selection_criteria="bic",
+                init_peak_detection_proximity_threshold=200,
+                include_scatter=False,
+                outlier_rejection_method="sigma_clip",
+                sigma_clip=3,
+                sigma_clip_max_iterations=2,
+                use_huber_loss=True,
+                fdr_alpha=0.01,
+                fdr_outlier_max_fraction=0.10,
+                extract_covariance=True,
+                fit_full_model=True,
+            )
+
     def test_clean_cube(self):
         aperture_radius = 4
         annulus_outer_radius = 5
@@ -198,7 +248,38 @@ class TestWorkflow:
             annulus_outer_radius=annulus_outer_radius,
             annulus_inner_radius=annulus_inner_radius,
             plot=False,
-            verbose=False,
+            verbose=True,
+            extract_uncertainty=True,
+            include_scatter=False,
+            outlier_rejection_method="fdr",
+            use_huber_loss=False,
+            fdr_alpha=0.01,
+            fdr_outlier_max_fraction=0.15,
+            sigma_clip_sigma=3,
+            sigma_clip_max_iterations=20,
+            num_samples_uncertainty_region=1000,
+        )
+        assert cleaned_cube.shape == self.data_cube.shape
+        assert cleaned_noise_cube.shape == self.data_cube.shape
+        assert cleaned_map.shape == self.data_cube[0].shape
+
+        cleaned_cube, cleaned_noise_cube, cleaned_map = self.wcleaner.clean_cube(
+            wiggle_detection_sigma_threshold=5.0,
+            wiggle_detection_variance_ratio_threshold=0.2,
+            n_amplitude=10,
+            n_frequency=7,
+            fit_full_model=True,
+            min_n_amplitude=None,
+            min_n_frequency=None,
+            n_amplitude_for_detection=10,
+            n_frequency_for_detection=7,
+            cleaning_mask=mask,
+            init_peak_detection_proximity_threshold=200,
+            aperture_radius=aperture_radius,
+            annulus_outer_radius=annulus_outer_radius,
+            annulus_inner_radius=annulus_inner_radius,
+            plot=False,
+            verbose=True,
             extract_uncertainty=True,
             include_scatter=False,
             outlier_rejection_method="fdr",
