409a3f4c96978bd36860bf5af87f5f0a7ce0b3b6
Anowar Shajib <ajshajib@uchicago.edu>
2025-07-17 00:36:29 -0500

    Bump version from 0.1.0rc1 to 0.1.0 in pyproject.toml, __init__.py, and setup.py

--- setup.py
+++ setup.py
@@ -34,6 +34,6 @@ setup(
     packages=find_packages(where=".", include=["raccoon", "raccoon.*"]),
     package_dir={"": "."},
     url="https://github.com/ajshajib/raccoon",
-    version="0.1.0rc1",
+    version="0.1.0",
     zip_safe=False,
 )
